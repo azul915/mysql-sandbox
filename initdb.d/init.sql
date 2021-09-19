c3e8f5f65cbe89455c7bec1ef00fdda8778eab68
azul915 <k.aoyagi940516@gmail.com>
2021-09-19 14:52:50 +0900

    update

--- initdb.d/init.sql
+++ initdb.d/init.sql
@@ -1,6 +1,16 @@
-CREATE DATABASE IF NOT EXISTS sandbox;
+CREATE DATABASE IF NOT EXISTS HR;
 
-CREATE TABLE IF NOT EXISTS sandbox.employee 
-(id int, name varchar(255), status varchar(255), join_day varchar(255), birthday varchar(255), email varchar(255), tel varchar(255), address varchar(255), private_tel varchar(255), grade varchar(255), belongs varchar(255));
+CREATE TABLE IF NOT EXISTS HR.employee (
+    id int, name varchar(255), status varchar(255), join_day varchar(255), 
+    birthday varchar(255), email varchar(255), tel varchar(255), address varchar(255), 
+    private_tel varchar(255), grade varchar(255), belongs varchar(255)
+);
 
-CREATE TABLE IF NOT EXISTS sandbox.department (id int, name varchar(255), boss_employee_id int, upper_department_id int);
+CREATE TABLE IF NOT EXISTS HR.department (
+    id int, name varchar(255), boss_employee_id int, upper_department_id int
+);
+
+DROP VIEW IF EXISTS HR.enter_admin;
+CREATE VIEW HR.enter_admin (id, name, grade, boss_name) AS
+SELECT T1.id, T1.name, T1.grade, T2.name
+FROM HR.employee T1, HR.employee T2, HR.department T3;
