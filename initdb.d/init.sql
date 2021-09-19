0b1856bbf5b4ae8eb409efb8b4b30c8b552a628f
azul915 <k.aoyagi940516@gmail.com>
2021-09-19 15:02:43 +0900

    update

--- initdb.d/init.sql
+++ initdb.d/init.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS HR;
 CREATE TABLE IF NOT EXISTS HR.employee (
     id int, name varchar(255), status varchar(255), join_day varchar(255), 
     birthday varchar(255), email varchar(255), tel varchar(255), address varchar(255), 
-    private_tel varchar(255), grade varchar(255), belongs varchar(255)
+    private_tel varchar(255), grade varchar(255), belongs_department_id varchar(255)
 );
 
 CREATE TABLE IF NOT EXISTS HR.department (
@@ -13,4 +13,6 @@ CREATE TABLE IF NOT EXISTS HR.department (
 DROP VIEW IF EXISTS HR.enter_admin;
 CREATE VIEW HR.enter_admin (id, name, grade, boss_name) AS
 SELECT T1.id, T1.name, T1.grade, T2.name
-FROM HR.employee T1, HR.employee T2, HR.department T3;
+FROM HR.employee T1, HR.employee T2, HR.department T3
+WHERE T2.id = T3.boss_employee_id AND T1.belongs_department_id = T3.id
+;
